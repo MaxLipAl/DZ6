2f51ebf571380de7cb32adc04bfc926f608a075f
Maxim <maxl08@list.ru>
2023-07-23 19:41:52 +0300

    DZ6

---
+++ 1/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int [] InputArray (int number)
+{
+    int [] array = new int[number];
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        int newNumber = Convert.ToInt32(Console.ReadLine());
+        array[i] = newNumber;
+    }
+    return array;
+} 
+
+int Result(int [] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) result++;
+    }
+    return result;
+}
+
+int numberM = GetInfo("Введите колличество чисел, которые хотите проверить: ");
+int [] array = InputArray(numberM);
+int result = Result(array);
+
+Console.WriteLine($"[{string.Join(", ", array)}] - > {result}");
+
+
---
+++ 2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void Point (double b1, double k1, double b2, double k2)
+{
+    double pointX = -(b1 - b2) / (k1 - k2);
+    double pointY = k1 * pointX + b1;
+    Console.Write($"{pointX}, {pointY}");
+}
+
+double GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double b1 = GetInfo("Введите координату b1: ");
+double k1 = GetInfo("Введите координату k1: ");
+double b2 = GetInfo("Введите координату b2: ");
+double k2 = GetInfo("Введите координату k2: ");
+
+Point(b1, k1, b2, k2);
